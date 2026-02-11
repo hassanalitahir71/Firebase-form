b1a8939b35c5c810e937a239a372811cf6e88531
Hassan Ali TAHIR <hassanalitahir71@gmail.com>
2026-02-11 09:15:35 -0800

    feat: integrate Firebase and add contact input UI

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import './App.css'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import NavBar from "./components/NavBar";
+import { IoSearchSharp } from "react-icons/io5";
+import { IoMdAddCircle } from "react-icons/io";
+import { collection } from "firebase/firestore";
+import { db } from "./config/firebase";
+import { getDocs } from "firebase/firestore";
+import "./App.css";
 function App() {
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        const contactSRef = collection(db, "contacts");
+        const snapshot = await getDocs(contactSRef);
+        const contactsData = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setContacts(contactsData);
+        console.log(contactsData);
+      } catch (error) {
+        console.error("Error fetching contacts:", error);
+      }
+    };
+    fetchContacts();
+  }, []);
+
   return (
-   <div className="bg-mint-500 h-150 w-92 p-4 flex flex-col items-center  ">
+    <div className="bg-mint-500 flex h-120 w-92 flex-col items-center rounded-2xl p-2">
       <NavBar />
-      
+      <div className="flex w-full items-center gap-1 py-1">
+        <div className="mt- align-center relative flex w-80 justify-center rounded-lg">
+          <IoSearchSharp className="absolute top-1/2 left-1 -translate-y-1/2 text-3xl text-white" />
+          <input
+            type="text"
+            placeholder="Search contacts..."
+            className="h-10 w-full rounded-lg border border-gray-300 p-2 pl-9 text-amber-50 focus:outline-none"
+          />
+        </div>
+        <IoMdAddCircle className="cursor-pointer text-5xl text-white" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- app/src/components/NavBar.jsx
+++ app/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/logos_firebase.png'
 function NavBar() {
   return (
     
-    <div className="bg-white w-full h-12 flex items-center justify-center gap-2 shadow-md rounded-lg">
+    <div className="bg-white w-88  h-12 flex items-center justify-center gap-4 shadow-md rounded-lg">
       <img src={logo} alt="firebase-logo" className="w-6 h-6" />
       <h1 className="text-lg font-bold text-gray-800">Firebase Contact Form</h1>
     </div>
